0f416327fe50e084568d3fe84b477014e1353026
Jedrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2023-04-18 13:54:19 +0200

    Initial commit

---
+++ run_experiments.py
@@ -0,0 +1,30 @@
+from pprint import pprint
+
+import numpy as np
+
+from .generator import RandomNumberGenerator
+
+
+def populate_distance_matrix(D, gen):
+    n = D.shape[0]
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            D[i, j] = gen.nextInt(1, 30)
+    return D
+
+
+def main():
+    seed = 42
+    gen = RandomNumberGenerator(seedVaule=seed)
+
+    N = 10  # liczba punktów
+    D = np.full(shape=(N, N), fill_value=np.nan)  # macierz odległości
+    D = populate_distance_matrix(D, N, gen)
+    
+    pprint(D)
+
+
+if __name__ == "__main__":
+    main()
